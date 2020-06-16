70fe66ab9e03874d89ebfa79365461c3ce1bc978
Yu Wang <32283316+yuwangme@users.noreply.github.com>
2020-06-16 08:38:33 -0700

    start coding class HYCHEM

--- src/hychem.py
+++ src/hychem.py
@@ -2,6 +2,11 @@ class CONDITION:
     ''' class for experimental conditions '''
 
     def __init__(self, T=None, P=None, MF={None: None}):
+        ''' inputs:
+            T: temperature (scalar)
+            P: pressure (scalar)
+            MF: mole fractions (dict)
+        '''
         self._T = T
         self._P = P
         self._MF = MF
@@ -17,6 +22,7 @@ class CONDITION:
         return self._MF
 
     def __repr__(self):
+        '''' print all conditions '''
         out = f"Temperature: {self._T} K\nPressure: {self._P} atm\n"
         for k, v in self._MF.items():
             out += f"{k}: {v} (fraction)\n"
@@ -33,5 +39,24 @@ class HYCHEM:
 class OPTIMIZER:
     ''' class for optimization of hychem parameters '''
 
-    def __init__(self):
+    def __init__(self, hychem, chemkin, algo):
+        ''' inputs
+            hychem: HyChem parameters
+            chemkin: CHEKMIN wrapper
+        '''
+        self.hychem = hychem
+        self.chemkin = chemkin
+        self.algo = algo
         return
+
+    def optimize(self):
+        ''' optimize '''
+        if self.algo == "coordinate_desc":
+            # coordinate descent
+            self._coordinate_desc()
+        else:
+            raise ValueError("invalid algorithm!")
+
+    def _coordinate_desc(self):
+        ''' coordinate descent '''
+        raise NotImplementedError("not implemented!")
